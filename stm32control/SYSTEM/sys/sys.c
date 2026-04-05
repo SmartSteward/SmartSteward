8d1ea61ade4188dd54f1c288d68783eb7b352b02
axgn <141894446+axgn@users.noreply.github.com>
2026-04-05 13:02:25 +0800

    更新依赖版本

--- stm32control/SYSTEM/sys/sys.c
+++ stm32control/SYSTEM/sys/sys.c
@@ -1,22 +1,5 @@
 #include "sys.h"
-/***********************************************
-公司：轮趣科技（东莞）有限公司
-品牌：WHEELTEC
-官网：wheeltec.net
-淘宝店铺：shop114407458.taobao.com
-速卖通: https://minibalance.aliexpress.com/store/4455017
-版本：V1.0
-修改时间：2023-01-04
 
-Brand: WHEELTEC
-Website: wheeltec.net
-Taobao shop: shop114407458.taobao.com
-Aliexpress: https://minibalance.aliexpress.com/store/4455017
-Version: V1.0
-Update：2023-01-04
-
-All rights reserved
-***********************************************/
 
 void WFI_SET(void) { __ASM volatile("wfi"); }
 // 关闭所有中断
